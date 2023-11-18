6c12ff154453402d0ef45ef0c01122c4ac7d2e7e
Vanderlei Aniecevski <kikovander@gmail.com>
2023-11-17 23:46:56 -0300

    Implantação da tela de login

---
+++ SendGen.Web/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendGen.Web.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SendGen.Web.Controllers
+{
+	public class LoginController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+        public IActionResult Entrar(LoginModel loginModel)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					if (loginModel.Login == "adm" && loginModel.Senha == "123")
+					{
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    TempData["MensagemErro"] = $"Usuário e/ou senha inválido(s). Tente novamente!";
+
+                }
+				return View("Index");
+			}
+
+			catch (Exception erro)
+			{
+				TempData["MensagemErro"] = $"Ops, não conseguimos realizar seu login, tente novamente. Detalhe do erro: {erro.Message}";
+				return RedirectToAction("Index");
+			}
+			
+		}
+	}
+}
---
+++ SendGen.Web/Models/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SendGen.Web.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Digite o usuário")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Digite o senha")]
+        public string Senha { get; set; }
+    }
+}
